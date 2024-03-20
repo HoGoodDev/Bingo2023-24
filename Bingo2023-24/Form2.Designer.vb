3d3719602117541d0e8275656df8b0beccd99bc7
CMP_HoGoodwill <CMP_HoGoodwill@RM37-7>
2024-03-20 10:34:13 -0400

    Add project files.

---
+++ Bingo2023-24/Form2.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class board
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(board))
+        Me.pnlBoard = New System.Windows.Forms.Panel()
+        Me.pnlCardTop = New System.Windows.Forms.Panel()
+        Me.SuspendLayout()
+        '
+        'pnlBoard
+        '
+        Me.pnlBoard.Location = New System.Drawing.Point(35, 115)
+        Me.pnlBoard.Name = "pnlBoard"
+        Me.pnlBoard.Size = New System.Drawing.Size(256, 260)
+        Me.pnlBoard.TabIndex = 0
+        '
+        'pnlCardTop
+        '
+        Me.pnlCardTop.Location = New System.Drawing.Point(35, 32)
+        Me.pnlCardTop.Name = "pnlCardTop"
+        Me.pnlCardTop.Size = New System.Drawing.Size(256, 58)
+        Me.pnlCardTop.TabIndex = 1
+        '
+        'board
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.ClientSize = New System.Drawing.Size(483, 521)
+        Me.Controls.Add(Me.pnlCardTop)
+        Me.Controls.Add(Me.pnlBoard)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "board"
+        Me.Text = "Form2"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pnlBoard As Panel
+    Friend WithEvents pnlCardTop As Panel
+End Class
---
+++ Bingo2023-24/Form2.vb
@@ -0,0 +1,88 @@
+﻿Imports System.Data.SqlClient
+Imports System.Runtime.Remoting.Metadata.W3cXsd2001
+
+Public Class board
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ResetBoard()
+
+
+    End Sub
+
+    Private Sub ResetBoard()
+
+        Dim btnWidth As Integer = pnlBoard.Width / 5
+        Dim btnHeight As Integer = pnlBoard.Height / 5
+        Dim rand As New Random()
+
+        For i As Integer = 0 To 4
+
+            Dim startpoint = (15 * i)
+            Dim possible As New List(Of Integer)
+
+            For k As Integer = 1 To 15
+
+                possible.Add(startpoint + k)
+
+            Next
+
+            For j As Integer = 0 To 4
+
+                Dim index As Integer = rand.Next(0, possible.Count)
+                Dim btn As New Button With {
+                .Text = possible(index).ToString,
+                .Location = New Point(btnWidth * i, btnHeight * j),
+                .Width = btnWidth,
+                .Height = btnHeight}
+
+                AddHandler btn.Click, AddressOf handlebuttonclick
+
+                pnlBoard.Controls.Add(btn)
+                possible.RemoveAt(index)
+
+
+            Next
+
+
+        Next
+
+    End Sub
+
+    Sub HandleButtonClick(sender As Button, e As EventArgs)
+
+        If sender.BackColor = Color.Yellow Then
+            sender.BackColor = Color.White
+        Else
+            sender.BackColor = Color.Yellow
+        End If
+
+    End Sub
+
+    Private Sub pnlCardTop_Paint(sender As Object, e As PaintEventArgs) Handles pnlCardTop.Paint
+
+
+
+        Dim letters() As String = {"B", "I", "N", "G", "O"}
+
+        Dim lblWidth As Integer = pnlCardTop.Width / 5
+
+        Dim lblHeight As Integer = pnlCardTop.Height
+
+        For i As Integer = 0 To 4
+
+            Dim lbl As New Label With {
+            .Text = letters(i),
+            .Font = New Font("Segoe UI", 40, FontStyle.Regular),
+            .TextAlign = ContentAlignment.MiddleCenter,
+            .Location = New Point(lblWidth * i, 0),
+            .Width = lblWidth,
+            .Height = lblHeight
+            }
+
+            pnlCardTop.Controls.Add(lbl)
+
+        Next
+
+    End Sub
+
+End Class
